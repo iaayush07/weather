6a265da7be1968fe7a26a0ba711461927633ebea
Ayush Dhimmar <ayush.dhimmar@1rivet.local>
2023-03-13 15:33:46 +0530

    ui of weather

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'weather', loadChildren: () => import('./weather/weather.module').then(m => m.WeatherModule) }];
+const routes: Routes = [{ path: 'weather', loadChildren: () => import('./weather/weather.module').then(m => m.WeatherModule) }, {
+  path: '',
+  pathMatch: 'full',
+  redirectTo: 'weather'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/weather/weather-routing.module.ts
+++ src/app/weather/weather-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WeatherContainerComponent } from './weather-container/weather-container.component';
 import { WeatherComponent } from './weather.component';
 
-const routes: Routes = [{ path: '', component: WeatherComponent }];
+const routes: Routes = [{
+  path: '', component: WeatherComponent,
+  children: [{
+    path: 'current',
+    component: WeatherContainerComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'current'
+  }
+  ]
+},];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
